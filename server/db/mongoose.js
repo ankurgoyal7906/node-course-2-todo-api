d0cb0f89952758ad18589fc9da9d6fb440f2ef5e
Ankur <ankurgoyal7906@gmail.com>
2018-03-07 18:56:18 +0530

    Setup ap for heroku manually using mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
   var mongoose = require('mongoose');
 
   mongoose.Promise = global.Promise;
-  mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+  mongoose.connect('mongodb://ankur33:aankur13047G@ds257858.mlab.com:57858/todoapp' || 'mongodb://localhost:27017/TodoApp');
 
   module.exports = {mongoose};
